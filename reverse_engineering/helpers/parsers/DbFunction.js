8f185c6a81446bebdad9497107bc8fd5b017b7b8
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2021-03-22 17:07:24 +0200

    fix parsing functions

--- reverse_engineering/helpers/parsers/DbFunction.js
+++ reverse_engineering/helpers/parsers/DbFunction.js
@@ -5,7 +5,7 @@ class DbFunction {
 	}
 
 	parse() {
-		const parseRegexp = /create(?<orReplace>\s+or\s+replace)?(?<definer>\s+definer\s*=[\s\S]+?)?(?<aggregate>\s+aggregate)?\s+function(?<ifNotExists>\s+if not exists)?\s+\`(?<funcName>[\s\S]+?)\`\s*\((?<funcParameters>[\s\S]+?)\)\s+returns\s+(?<returnType>[a-z0-9\(\)]+)(?<characteristiсs>[\s\S]+?)?\s+(?<funcBody>return\s+\(([\s\S]+)\)|(BEGIN[\s\S]+END))/i;
+		const parseRegexp = /create(?<orReplace>\s+or\s+replace)?(?<definer>\s+definer\s*=[\s\S]+?)?(?<aggregate>\s+aggregate)?\s+function(?<ifNotExists>\s+if not exists)?\s+\`(?<funcName>[\s\S]+?)\`\s*\((?<funcParameters>[\s\S]*?)\)\s+returns\s+(?<returnType>[a-z0-9\(\)]+)(?<characteristics>(\s*language\s+sql)?(\s*(not)?\s+deterministic)?(\s*contains\s+(sql|no\s+sql|reads\s+sql\s+data|modifies\s+sql\s+data))?(\s*sql\s+security\s+(definer|invoker))?(\s*comment\s+\'[\s\S]+?\')?(\s*charset\s+[\S\s]+?)?(\s*COLLATE\s+[\S\s]+?)?)?\s+(?<funcBody>(begin|return)([\s\S]+))/i;
 
 		if (!parseRegexp.test(this.query)) {
 			return;
@@ -20,7 +20,7 @@ class DbFunction {
 			funcName,
 			funcParameters,
 			returnType,
-			characteristiсs,
+			characteristics,
 			funcBody,
 		} = result.groups;
 
@@ -31,18 +31,18 @@ class DbFunction {
 		this.name = funcName;
 		this.parameters = funcParameters;
 		this.returnType = returnType;
-		this.characteristiсs = characteristiсs || '';
+		this.characteristics = characteristics || '';
 		this.body = funcBody || '';
 	}
 
 	getLanguage() {
-		return /language sql/i.test(this.characteristiсs) ? 'SQL' : '';
+		return /language sql/i.test(this.characteristics) ? 'SQL' : '';
 	}
 
 	getDeterministic() {
-		if (/not deterministic/i.test(this.characteristiсs)) {
+		if (/not deterministic/i.test(this.characteristics)) {
 			return 'NOT DETERMINISTIC';
-		} else if (/deterministic/i.test(this.characteristiсs)) {
+		} else if (/deterministic/i.test(this.characteristics)) {
 			return 'DETERMINISTIC';
 		} else {
 			return '';
@@ -50,13 +50,13 @@ class DbFunction {
 	}
 
 	getContains() {
-		if (/contains\s+sql/i.test(this.characteristiсs)) {
+		if (/contains\s+sql/i.test(this.characteristics)) {
 			return 'SQL';
-		} else if (/contains\s+no\s+sql/i.test(this.characteristiсs)) {
+		} else if (/contains\s+no\s+sql/i.test(this.characteristics)) {
 			return 'NO SQL';
-		} else if (/contains\s+reads\s+sql\s+data/i.test(this.characteristiсs)) {
+		} else if (/contains\s+reads\s+sql\s+data/i.test(this.characteristics)) {
 			return 'READS SQL DATA';
-		} else if (/contains\s+modifies\s+sql\s+data/i.test(this.characteristiсs)) {
+		} else if (/contains\s+modifies\s+sql\s+data/i.test(this.characteristics)) {
 			return 'MODIFIES SQL DATA';
 		} else {
 			return '';
@@ -64,9 +64,9 @@ class DbFunction {
 	}
 
 	getDefiner() {
-		if (/SQL\s+SECURITY\s+DEFINER/i.test(this.characteristiсs)) {
+		if (/SQL\s+SECURITY\s+DEFINER/i.test(this.characteristics)) {
 			return 'DEFINER';
-		} else if (/SQL\s+SECURITY\s+INVOKER/i.test(this.characteristiсs)) {
+		} else if (/SQL\s+SECURITY\s+INVOKER/i.test(this.characteristics)) {
 			return 'INVOKER';
 		} else {
 			return '';
@@ -76,11 +76,11 @@ class DbFunction {
 	getComment() {
 		const commentRegexp = /comment\s\'([\s\S]+?)\'/i;
 
-		if (!commentRegexp.test(this.characteristiсs)) {
+		if (!commentRegexp.test(this.characteristics)) {
 			return '';
 		}
 
-		const result = this.characteristiсs.match(commentRegexp);
+		const result = this.characteristics.match(commentRegexp);
 
 		return result[1] || '';
 	}
