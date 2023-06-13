1d7edd67d39d43c86c27c6ebbcaf8524c107d0c5
unknown <unknown>
2023-06-13 10:15:43 +0300

    Created table options enum

--- forward_engineering/helpers/general.js
+++ forward_engineering/helpers/general.js
@@ -1,81 +1,6 @@
+const {TableOptions, TableOptionsByEngine} = require("../enums/tableOptions");
+
 module.exports = (_, wrap) => {
-	const OPTION_KEYWORDS = {
-		ENGINE: 'ENGINE',
-		AUTO_INCREMENT: 'AUTO_INCREMENT',
-		AVG_ROW_LENGTH: 'AVG_ROW_LENGTH',
-		CHECKSUM: 'CHECKSUM',
-		DATA_DIRECTORY: 'DATA DIRECTORY',
-		DELAY_KEY_WRITE: 'DELAY_KEY_WRITE',
-		INDEX_DIRECTORY: 'INDEX DIRECTORY',
-		ENCRYPTED: 'ENCRYPTED',
-		ENCRYPTION_KEY_ID: 'ENCRYPTION_KEY_ID',
-		IETF_QUOTES: 'IETF_QUOTES',
-		INSERT_METHOD: 'INSERT_METHOD',
-		UNION: 'UNION',
-		KEY_BLOCK_SIZE: 'KEY_BLOCK_SIZE',
-		MIN_ROWS: 'MIN_ROWS',
-		MAX_ROWS: 'MAX_ROWS',
-		PACK_KEYS: 'PACK_KEYS',
-		PAGE_CHECKSUM: 'PAGE_CHECKSUM',
-		PAGE_COMPRESSED: 'PAGE_COMPRESSED',
-		PAGE_COMPRESSION_LEVEL: 'PAGE_COMPRESSION_LEVEL',
-		ROW_FORMAT: 'ROW_FORMAT',
-		SEQUENCE: 'SEQUENCE',
-		STATS_AUTO_RECALC: 'STATS_AUTO_RECALC',
-		STATS_PERSISTENT: 'STATS_PERSISTENT',
-		TRANSACTIONAL: 'TRANSACTIONAL',
-		WITH_SYSTEM_VERSIONING: 'WITH SYSTEM VERSIONING',
-	};
-
-	const OPTIONS_BY_ENGINE = {
-		MyISAM: [
-			'AUTO_INCREMENT',
-			'AVG_ROW_LENGTH',
-			'CHECKSUM',
-			'DATA_DIRECTORY',
-			'DELAY_KEY_WRITE',
-			'INDEX_DIRECTORY',
-			'KEY_BLOCK_SIZE',
-			'PACK_KEYS',
-			'ROW_FORMAT',
-			'WITH_SYSTEM_VERSIONING',
-		],
-		InnoDB: [
-			'AUTO_INCREMENT',
-			'DATA_DIRECTORY',
-			'INDEX_DIRECTORY',
-			'ENCRYPTED',
-			'ENCRYPTION_KEY_ID',
-			'KEY_BLOCK_SIZE',
-			'PACK_KEYS',
-			'PAGE_COMPRESSED',
-			'PAGE_COMPRESSION_LEVEL',
-			'ROW_FORMAT',
-			'SEQUENCE',
-			'STATS_AUTO_RECALC',
-			'STATS_PERSISTENT',
-			'WITH_SYSTEM_VERSIONING',
-		],
-		CSV: ['IETF_QUOTES', 'KEY_BLOCK_SIZE', 'PACK_KEYS', 'WITH_SYSTEM_VERSIONING'],
-		MERGE: ['INSERT_METHOD', 'UNION', 'KEY_BLOCK_SIZE', 'PACK_KEYS', 'WITH_SYSTEM_VERSIONING'],
-		Aria: [
-			'AUTO_INCREMENT',
-			'AVG_ROW_LENGTH',
-			'CHECKSUM',
-			'DATA_DIRECTORY',
-			'DELAY_KEY_WRITE',
-			'INDEX_DIRECTORY',
-			'PAGE_CHECKSUM',
-			'ROW_FORMAT',
-			'KEY_BLOCK_SIZE',
-			'PACK_KEYS',
-			'TRANSACTIONAL',
-			'WITH_SYSTEM_VERSIONING',
-		],
-		Memory: ['AUTO_INCREMENT', 'KEY_BLOCK_SIZE', 'PACK_KEYS', 'WITH_SYSTEM_VERSIONING'],
-		Archive: ['AUTO_INCREMENT', 'KEY_BLOCK_SIZE', 'PACK_KEYS', 'WITH_SYSTEM_VERSIONING'],
-	};
-
 	const getTableName = (tableName, schemaName) => {
 		if (schemaName) {
 			return `\`${schemaName}\`.\`${tableName}\``;
@@ -136,12 +61,12 @@ module.exports = (_, wrap) => {
 			tableOptions.push(`COMMENT = '${encodeStringLiteral(options.description)}'`);
 		}
 
-		const optionKeywords = OPTIONS_BY_ENGINE[engine] || ['KEY_BLOCK_SIZE', 'PACK_KEYS', 'WITH_SYSTEM_VERSIONING'];
+		const optionKeywords = TableOptionsByEngine[engine] || ['KEY_BLOCK_SIZE', 'PACK_KEYS', 'WITH_SYSTEM_VERSIONING'];
 
 		optionKeywords.forEach(keyword => {
 			if (keyword === 'WITH_SYSTEM_VERSIONING') {
 				if (options[keyword]) {
-					return tableOptions.push(OPTION_KEYWORDS[keyword]);
+					return tableOptions.push(TableOptions[keyword]);
 				} else {
 					return;
 				}
@@ -153,7 +78,7 @@ module.exports = (_, wrap) => {
 				return;
 			}
 
-			const option = `${OPTION_KEYWORDS[keyword]} = ${value}`;
+			const option = `${TableOptions[keyword]} = ${value}`;
 
 			tableOptions.push(option);
 		});
