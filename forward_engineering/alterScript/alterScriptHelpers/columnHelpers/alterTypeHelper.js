0b8c536b490f0c20f69062f5d141d3987ebbce11
unknown <unknown>
2023-06-28 15:35:46 +0300

    Fixed problem with type attributes not being taken into account when updating column types

--- forward_engineering/alterScript/alterScriptHelpers/columnHelpers/alterTypeHelper.js
+++ forward_engineering/alterScript/alterScriptHelpers/columnHelpers/alterTypeHelper.js
@@ -1,6 +1,42 @@
 const {AlterScriptDto} = require("../../types/AlterScriptDto");
 
 /**
+ * @return {boolean}
+ * */
+const hasLengthChanged = (collection, oldFieldName, currentJsonSchema) => {
+    const oldProperty = collection.role.properties[oldFieldName];
+
+    const previousLength = oldProperty?.length;
+    const newLength = currentJsonSchema?.length;
+    return previousLength !== newLength;
+}
+
+/**
+ * @return {boolean}
+ * */
+const hasPrecisionOrScaleChanged = (collection, oldFieldName, currentJsonSchema) => {
+    const oldProperty = collection.role.properties[oldFieldName];
+
+    const previousPrecision = oldProperty?.precision;
+    const newPrecision = currentJsonSchema?.precision;
+    const previousScale = oldProperty?.scale;
+    const newScale = currentJsonSchema?.scale;
+
+    return previousPrecision !== newPrecision || previousScale !== newScale;
+}
+
+/**
+ * @return {boolean}
+ * */
+const hasMicroSecPrecisionChanged = (collection, oldFieldName, currentJsonSchema) => {
+    const oldProperty = collection.role.properties[oldFieldName];
+
+    const previousMicroSecPrecision = oldProperty?.microSecPrecision;
+    const newMicroSecPrecision = currentJsonSchema?.microSecPrecision;
+    return previousMicroSecPrecision !== newMicroSecPrecision;
+}
+
+/**
  * @return {(collection: Object) => AlterScriptDto[]}
  * */
 const getUpdateTypesScriptDtos = (_, ddlProvider) => (collection) => {
@@ -21,7 +57,17 @@ const getUpdateTypesScriptDtos = (_, ddlProvider) => (collection) => {
     const dbData = {databaseName};
 
     return _.toPairs(collection.properties)
-        .filter(([name, jsonSchema]) => checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode']))
+        .filter(([name, jsonSchema]) => {
+            const hasTypeChanged = checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode'])
+            if (!hasTypeChanged) {
+                const oldName = jsonSchema.compMod.oldField.name;
+                const isNewLength = hasLengthChanged(collection, oldName, jsonSchema);
+                const isNewPrecisionOrScale = hasPrecisionOrScaleChanged(collection, oldName, jsonSchema);
+                const isNewMicroSecPrecision = hasMicroSecPrecisionChanged(collection, oldName, jsonSchema);
+                return isNewLength || isNewPrecisionOrScale || isNewMicroSecPrecision;
+            }
+            return hasTypeChanged;
+        })
         .map(([name, jsonSchema]) => {
             const dropColumnDdlName = wrapInTics(name);
             const dropScript = ddlProvider.dropColumn(fullName, dropColumnDdlName);
