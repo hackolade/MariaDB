bda610705455a035978e6817ce495a00e5ffd567
Yevhenii Moroziuk <yevhen.moroziuk@hackolade.com>
2025-05-16 11:23:35 +0300

    Revert "HCK-11091: Move the log of the start to the studio (#69)" (#70)
    This reverts commit 02eb5a25773b8f38e472efde8e5aa846a0890ac0.

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const connectionHelper = require('./helpers/connectionHelper');
 const mariadbHelper = require('./helpers/mariadbHelper');
 
@@ -31,6 +33,9 @@ module.exports = {
 		});
 
 		try {
+			logger.clear();
+			logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
+
 			const connection = await this.connect(connectionInfo, app);
 			const instance = connectionHelper.createInstance(connection, logger);
 
@@ -74,6 +79,8 @@ module.exports = {
 		});
 
 		try {
+			logger.clear();
+			logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
 			const systemDatabases = connectionInfo.includeSystemCollection
 				? []
 				: ['information_schema', 'mysql', 'performance_schema'];
@@ -135,6 +142,8 @@ module.exports = {
 		});
 
 		try {
+			logger.log('info', data, 'data', data.hiddenKeys);
+
 			const collections = data.collectionData.collections;
 			const dataBaseNames = data.collectionData.dataBaseNames;
 			const connection = await this.connect(data, app);
